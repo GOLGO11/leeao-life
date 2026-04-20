33dcfdc10e3011380d890b703ee16cd930e0e3a6
GOLGO11 <873200646@qq.com>
2026-04-20 23:29:14 +0800

    第6本书第9轮结束

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const dataFiles = [
   "./data/timeline-events-fifth-book.json",
   "./data/timeline-events-sixth-book.json"
 ];
-const dataVersion = "2026-04-20-sixth-round-8";
+const dataVersion = "2026-04-20-sixth-round-9";
 
 const dateFormatter = new Intl.DateTimeFormat("zh-Hant", {
   year: "numeric",
