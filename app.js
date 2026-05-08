41b2a4c21364a730e0ebc34ca3ad985dcb17a228
GOLGO11 <873200646@qq.com>
2026-05-08 13:27:57 +0800

    一个预备军官的日记 第80轮结束

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const supplementalDataFiles = new Set([
   "./data/timeline-events-fourth-book-deepening.json"
 ]);
 const processedBookCount = dataFiles.filter((file) => !supplementalDataFiles.has(file)).length;
-const dataVersion = "2026-05-08-preofficer-diary-round72-march-2";
+const dataVersion = "2026-05-08-preofficer-diary-round80-march-9-orders-leadership-money";
 
 const dateFormatter = new Intl.DateTimeFormat("zh-Hant", {
   year: "numeric",
